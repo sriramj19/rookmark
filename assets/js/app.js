1b15abf67f51eccb1e977d94083aa9eb84cc2ddc
New Mac <Newmac@HPs-MacBook-Air.local>
2017-05-30 15:06:38 +0530

    New states created

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,22 +7,44 @@ angular.module('app', [
     'oc.lazyLoad'
   ])
   .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('/', '/list'),
-    $urlRouterProvider.otherwise('/list'),
+    $urlRouterProvider.when('/', '/login'),
+    $urlRouterProvider.otherwise('/login'),
 
     $stateProvider.state('base', {
         'abstract': !0,
         url: '',
         templateUrl: 'views/base.html'
     })
-    .state('list', {
-        url: '/list',
+    .state('login', {
+        url: '/login',
         parent: 'base',
-        templateUrl: 'views/list.html',
+        templateUrl: 'views/login.html',
+        controller: function($localStorage, $state) {
+          if($localStorage.userDetails) {
+            $state.go('dashboard');
+          }
+        },
         resolve: { login: function($ocLazyLoad) {
           return $ocLazyLoad.load({
             name: 'app',
-            files: ['js/controllers/ListsController.js', 'js/services/lists.js']
+            files: ['js/controllers/UsersController.js', 'js/services/Users.js']
+          })
+        }
+      }
+    })
+    .state('dashboard', {
+        url: '/dashboard',
+        parent: 'base',
+        templateUrl: 'views/dashboard.html',
+        controller: function($localStorage, $state) {
+          if(!$localStorage.userDetails) {
+            $state.go('login');
+          }
+        },
+        resolve: { dashboard: function($ocLazyLoad) {
+          return $ocLazyLoad.load({
+            name: 'app',
+            files: ['js/controllers/BookmarksController.js', 'js/services/Bookmarks.js']
           })
         }
       }
