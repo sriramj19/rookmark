3db424decd3e36b1401a7e259a67eb65204b7d67
New Mac <Newmac@HPs-MacBook-Air.local>
2017-05-30 17:19:21 +0530

    APIs for bookmark activities added

--- assets/js/services/Bookmarks.js
+++ assets/js/services/Bookmarks.js
@@ -5,6 +5,37 @@ var app = angular.module('app');
 
 app.factory('BookmarksService', function($http) {
   return {
-
+    //API for viewing bookmarks
+    viewBookmarks: function(url, id) {
+      return $http.get(url + 'bookmarks/read/' + id).then(function(response) {
+        if(response) {
+          return response.data;
+        }
+      }, function(x) {
+        console.log(x.data.error);
+      });
+    },
+    //API for adding new bookmarks
+    addNewBookmark: function(apiURL, name, url, tags, id) {
+      return $http.post(apiURL + 'bookmarks/create', {name: name, url: url, tags: tags, user: id}).then(function(response) {
+        if(response) {
+          console.log(response.data);
+          return response.data;
+        }
+      }, function(x) {
+        alert(x.data.error);
+      });
+    },
+    //API for searching bookmarks
+    searchBookmark: function(url, tag, id) {
+      return $http.post(url + 'bookmarks/search', {tag : tag, id: id}).then(function(response) {
+        if(response) {
+          console.log(response);
+          return response.data;
+        }
+      }, function(x) {
+        alert(x.data.error);
+      });
+    },
   }
 });
