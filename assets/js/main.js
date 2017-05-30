71e0b76efa4912a67bec9c7a8e4e47240a479749
New Mac <Newmac@HPs-MacBook-Air.local>
2017-05-30 15:07:44 +0530

    Application functions updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
 angular.module('app')
-  .controller('AppCtrl', ['$scope', '$state', function($scope, $state) {
+  .controller('AppCtrl', ['$scope', '$state', '$localStorage', function($scope, $state, $localStorage) {
 
     $scope.app = {
-      name: 'Angular-Starter',
-      version: '1.0.0'
+      name: 'rookmark',
+      version: '1.0.0',
+      apiURL: 'http://' + location.host + '/',
+      userDetails: {},
+      landing: function() {
+        if($localStorage.userDetails) {
+          $state.go('dashboard')
+        }
+        else {
+          $state.go('login')
+        }
+      },
+      logout: function() {
+        $localStorage.$reset();
+        delete $scope.app.userDetails;
+        $state.go('login');
+      }
     }
 
 }]);
