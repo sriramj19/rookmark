a5e6e04d8297f328db78e37496a89cb679378599
New Mac <Newmac@HPs-MacBook-Air.local>
2017-05-30 17:19:50 +0530

    API calls for users modified

--- assets/js/services/Users.js
+++ assets/js/services/Users.js
@@ -5,19 +5,21 @@ var app = angular.module('app');
 
 app.factory('UsersService', function($http) {
   return {
+    //API for signin
     signin: function(url, email, password) {
       return $http.post(url + 'users/signin', {emailAddress: email, password: password}).then(function(response) {
         if(response) {
-          return response;
+          return response.data;
         }
       }, function(x) {
         alert(x.data.error);
       });
     },
+    //API for signup
     signup: function(url, email, password) {
       return $http.post(url + 'users/signup', {emailAddress: email, password: password}).then(function(response) {
         if(response) {
-          return response;
+          return response.data;
         }
       }, function(x) {
         alert(x.data.error);
